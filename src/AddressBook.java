bb813c785e6cbab3b2f08975749711d739109698
Andrew <andrew.x.l815@gmail.com>
2024-09-18 14:33:43 -0400

    made it

---
+++ src/AddressBook.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> addresses;
+    public AddressBook(){
+        addresses = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo b){
+        addresses.add(b);
+    }
+    public void removeBuddy(BuddyInfo b){
+        addresses.remove(b);
+    }
+
+    public static void main(String[] args){
+        AddressBook a = new AddressBook();
+        System.out.println("Address Book");
+        BuddyInfo b = new BuddyInfo();
+        a.addBuddy(b);
+        System.out.println(a);
+        a.removeBuddy(b);
+
+    }
+}
